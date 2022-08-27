399f8887f8b8b9ffe9eceb6ba06bb15fecb9a240
dhythm <yuta.okada.20@gmail.com>
2022-08-27 21:34:07 +0900

    implement form with zod and chakra

--- src/components/FormSample.tsx
+++ src/components/FormSample.tsx
@@ -3,9 +3,12 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import {
   Box,
+  FormControl,
   FormErrorMessage,
   FormLabel,
   Input,
+  NumberInput,
+  useNumberInput,
   VStack,
 } from "@chakra-ui/react";
 import { Button, ButtonGroup } from "@chakra-ui/react";
@@ -17,7 +20,7 @@ interface IFormInputs {
 
 const schema = z.object({
   firstName: z.string().min(1),
-  age: z.number().positive().int(),
+  age: z.preprocess((v) => Number(v), z.number().positive().int()),
 });
 
 export const FormSample = () => {
@@ -31,35 +34,42 @@ export const FormSample = () => {
   });
   const onSubmit = (data: IFormInputs) => console.log(data);
 
+  const { getInputProps } = useNumberInput({ step: 1 });
+  const input = getInputProps();
+
+  console.log({ errors });
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <VStack spacing={4}>
-        <Box>
-          <FormLabel>First Name</FormLabel>
-          <Controller
-            name="firstName"
-            control={control}
-            rules={{ required: true }}
-            render={({ field }) => <Input {...field} />}
-          />
-          <FormErrorMessage>{errors.firstName?.message}</FormErrorMessage>
-        </Box>
+      <FormControl isInvalid={Object.entries(errors).length !== 0}>
+        <VStack spacing={4}>
+          <Box>
+            <FormLabel>First Name</FormLabel>
+            <Controller
+              name="firstName"
+              control={control}
+              // rules={{ required: true }}
+              render={({ field }) => <Input {...field} />}
+            />
+            <FormErrorMessage>{errors.firstName?.message}</FormErrorMessage>
+          </Box>
 
-        <Box>
-          <FormLabel>Age</FormLabel>
-          <Controller
-            name="age"
-            control={control}
-            rules={{ required: true }}
-            render={({ field }) => <Input {...field} />}
-          />
-          <FormErrorMessage>{errors.age?.message}</FormErrorMessage>
-        </Box>
+          <Box>
+            <FormLabel>Age</FormLabel>
+            <Controller
+              name="age"
+              control={control}
+              // rules={{ required: true }}
+              render={({ field }) => <Input {...input} {...field} />}
+            />
+            <FormErrorMessage>{errors.age?.message}</FormErrorMessage>
+          </Box>
 
-        <Button type="submit" width="100%">
-          submit
-        </Button>
-      </VStack>
+          <Button type="submit" width="100%">
+            submit
+          </Button>
+        </VStack>
+      </FormControl>
     </form>
   );
 };
